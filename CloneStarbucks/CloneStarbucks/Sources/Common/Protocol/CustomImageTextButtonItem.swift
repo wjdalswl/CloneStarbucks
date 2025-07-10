45bbda58cc45a17e9101df1910c8c1e849a33294
jeongminji <parapin2274@gmail.com>
2025-07-11 06:47:10 +0900

    ✨ [Feat] #15 - CustomImageTextButton 컴포넌트 구현

---
+++ CloneStarbucks/CloneStarbucks/Sources/Common/Protocol/CustomImageTextButtonItem.swift
@@ -0,0 +1,13 @@
+//
+//  CustomImageTextButtonItem.swift
+//  CloneStarbucks
+//
+//  Created by jeongminji on 7/11/25.
+//
+
+import SwiftUI
+
+protocol CustomImageTextButtonItem {
+    var title: String { get }
+    var icon: Image { get }
+}
